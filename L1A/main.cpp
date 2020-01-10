21d74b5368b944e38588251fee59512c26cda2ee
Brendan <brendanuebelhoer@gmail.com>
2020-01-10 14:10:32 -0700

    First lab

---
+++ L1A/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // this is a comment its just for me the following lines display hello world to the user
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
